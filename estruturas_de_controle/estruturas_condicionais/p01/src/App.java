7a62f3ce1ee26692f70aa378783cc69ba843ddb4
Lucas Soares <polonio230@hotmail.com>
2023-07-08 16:55:55 -0300

    problema resolvido

--- estruturas_de_controle/estruturas_condicionais/p01/src/App.java
+++ estruturas_de_controle/estruturas_condicionais/p01/src/App.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        Scanner scanner = new Scanner(System.in);
+        double renda, IR=0, valorDeTaxa1, valorDeTaxa2, valorDeTaxa3;
+        renda = scanner.nextDouble();
+        if (renda <= 2000.00) {
+            System.out.println("Isento");
+        } else if (renda <= 3000.00) {
+            valorDeTaxa1 = renda - 2000.00;
+            IR = valorDeTaxa1 * 0.08;
+        } else if (renda <= 4500.00) {
+            valorDeTaxa2 = renda - 3000.00;
+            IR = 999.99 * 0.08 + valorDeTaxa2 * 0.18;
+        } else {
+            valorDeTaxa3 = renda - 4500.00;
+            IR = 999.99 * 0.08 + 1499.99 * 0.18 + valorDeTaxa3 * 0.28;
+        }
+        scanner.close();
+        if(renda>2000.00)
+            System.out.printf("R$ %.2f\n", IR);
     }
 }
