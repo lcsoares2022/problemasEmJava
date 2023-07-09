94e158a9eee9749a2ad0fb4f034039388a89df98
Lucas Soares <polonio230@hotmail.com>
2023-07-08 22:11:15 -0300

    Problema quase resolvido
    Alguns resultados de testes se mostraram inapropriados.

--- estruturas_de_controle/estruturas_condicionais/p02/src/App.java
+++ estruturas_de_controle/estruturas_condicionais/p02/src/App.java
@@ -1,26 +1,21 @@
 import java.util.Scanner;
-
 public class App {
     public static void main(String[] args) throws Exception {
-        String horaInicio, horaFim;
-        double qtdHorasIniciais, qtdHorasFinais, horaTotal;
+        String horaInicio, horaFim, horasJogando;
+        double qtdHorasIniciais, qtdHorasFinais, tempoTotal;
         Scanner scanner = new Scanner(System.in);
+        System.out.print("Digite o horário de início: ");
         horaInicio = scanner.nextLine();
-        qtdHorasIniciais = calcHora(horaInicio);
+        System.out.print("Digite o horário do fim: ");
         horaFim = scanner.nextLine();
-        qtdHorasFinais = calcHora(horaFim);
-        horaTotal = Math.abs(qtdHorasFinais-qtdHorasIniciais);
-        
-
-        
-        
-        
-        
-        
-        
         scanner.close();
+        qtdHorasIniciais = calcQtdHora(horaInicio);
+        qtdHorasFinais = calcQtdHora(horaFim);
+        tempoTotal = duracaoDoJogo(qtdHorasIniciais, qtdHorasFinais);
+        horasJogando = formatoHora(tempoTotal);
+        System.out.println("O JOGO DUROU " + horasJogando);     
     }
-    public static double calcHora(String horario) {
+    public static double calcQtdHora(String horario) {
         String[] partes = horario.split(":");
         int hora = Integer.parseInt(partes[0]);
         int minuto = Integer.parseInt(partes[1]);
@@ -30,4 +25,42 @@ public class App {
         double horaTotal = hora + minutoParaHora + segundoParaHora;
         return horaTotal;
     }
+    public static double duracaoDoJogo(double qtdHorasIniciais, double qtdHorasFinais) {
+        double tempoTotal=0, aux;
+        if(qtdHorasIniciais < 12.00 && qtdHorasFinais > 12){
+            aux = 12.00 - qtdHorasIniciais;
+            tempoTotal += aux;
+            aux = qtdHorasFinais - 12.00;
+            tempoTotal += aux;
+        } else if (qtdHorasIniciais > 12.00 && qtdHorasFinais < 12.00) {
+            aux = 24.00 - qtdHorasIniciais;
+            tempoTotal += (aux+qtdHorasFinais);
+        } else if (qtdHorasIniciais < 12.00 && qtdHorasFinais < 12.00 && qtdHorasFinais > qtdHorasIniciais) {
+            tempoTotal += (qtdHorasFinais-qtdHorasIniciais);
+        } else if (qtdHorasIniciais < 12.00 && qtdHorasFinais < 12.00 && qtdHorasFinais < qtdHorasIniciais) {
+            aux = 24.00 - qtdHorasIniciais;
+            tempoTotal += (aux+qtdHorasFinais);
+        } else if(qtdHorasIniciais > 12.00 && qtdHorasFinais > 12.00 && qtdHorasFinais > qtdHorasIniciais) {
+             tempoTotal += (qtdHorasFinais-qtdHorasIniciais);
+        } else if(qtdHorasIniciais > 12.00 && qtdHorasFinais > 12.00 && qtdHorasFinais < qtdHorasIniciais) {
+            aux = 24.00 - qtdHorasIniciais;
+            tempoTotal += aux;
+            aux = 24.00 - 12.00;
+            tempoTotal += aux;
+            aux = qtdHorasFinais - 12.00;
+            tempoTotal += aux; 
+        } else if (qtdHorasFinais == qtdHorasIniciais) {
+            tempoTotal = 24.00;
+        }       
+        return tempoTotal;
+    }
+
+    public static String formatoHora(double qtdHoras){
+        int hora = (int) qtdHoras;
+        int minuto = (int) (qtdHoras - hora) * 60;
+        int segundo = (int) ((qtdHoras - hora - minuto / 60.0) * 3600);
+        String horaFormatada = String.format("%02d:%02d:%02d", hora, minuto, segundo);
+        return horaFormatada;
+    }
+
 }
