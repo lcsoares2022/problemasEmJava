33879b965832791c7a9e57eb5f8c90f096158e40
Lucas Soares <polonio230@hotmail.com>
2023-07-11 16:17:38 -0300

    Problema de fatorial resolvido
    A inicialização de fatorial em 1 foi necessária para considerar o caso em que fatorial de 0 resulta em 1. Além disso, a variável de iteração foi iniciada com um valor maior que fatorial afim de evitar ambiguidade no cálculo.

--- estruturas_de_controle/estruturas_repetitivas/p04/src/App.java
+++ estruturas_de_controle/estruturas_repetitivas/p04/src/App.java
@@ -1,5 +1,14 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        int N, fatorial;
+        Scanner scanner = new Scanner(System.in);
+        N = scanner.nextInt();
+        fatorial = 1;
+        for(int i = fatorial+1; i<=N; i++)
+              fatorial *= i;
+        scanner.close();
+        System.out.println(fatorial);
     }
 }
