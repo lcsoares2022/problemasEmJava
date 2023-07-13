683a85a5a5eb900d7d8d081d028078cc2c6e516d
Lucas Soares <polonio230@hotmail.com>
2023-07-13 14:37:27 -0300

    Resolvendo o problema
    Foi utilizado expressão condicional ternária afim de testar sua utilidade.

--- POO/p06/README.md
+++ POO/p06/README.md
@@ -1,4 +1,4 @@
-# Comparação de áreas entre triângulos
+# Comparação de áreas entre triângulos (SEM POO)
 
 Fazer um programa para ler as medidas dos lados de dois triângulos X e Y (suponha medidas
 válidas). Em seguida, mostrar o valor das áreas dos dois triângulos e dizer qual dos dois triângulos possui a maior área. Utilize a fórmula de Heron para calcular a área.
--- POO/p06/src/App.java
+++ POO/p06/src/App.java
@@ -1,5 +1,30 @@
+import java.util.Locale;
+import java.util.Scanner;
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Locale.setDefault(Locale.US);
+        double l1, l2, l3, areaX, areaY, p;
+        String maior;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter the measures of triangle X:");
+        l1 = scanner.nextDouble();
+        l2 = scanner.nextDouble();
+        l3 = scanner.nextDouble();
+        p = (l1 + l2 + l3) / 2.0;
+        areaX = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
+        System.out.println("Enter the measures of triangle Y:");
+        l1 = scanner.nextDouble();
+        l2 = scanner.nextDouble();
+        l3 = scanner.nextDouble();
+        p = (l1 + l2 + l3) / 2.0;
+        areaY = Math.sqrt(p*(p-l1)*(p-l2)*(p-l3));
+        System.out.printf("Triangle X area: %.4f\n",areaX);
+        System.out.printf("Triangle Y area: %.4f\n",areaY);
+        maior = areaX > areaY ? "Larger area: X"
+                : areaX < areaY ? "Larger area: Y"
+                : "area X and Y is the same";
+        System.out.println(maior);
+        scanner.close();
+
     }
 }
