f635d24f240c1d79e3e7331de2d005e981700792
Lucas Soares <polonio230@hotmail.com>
2023-07-14 11:06:10 -0300

    Rearranjando e organizando linhas na classe

--- POO/p07/src/App.java
+++ POO/p07/src/App.java
@@ -5,6 +5,8 @@ import classes.Triangle;
 public class App {
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
+		System.out.println("teste");
+		double p, areaX, areaY;
 		Scanner sc = new Scanner(System.in);
         Triangle x, y;
         x = new Triangle();
@@ -17,12 +19,12 @@ public class App {
 		y.l1 = sc.nextDouble();
 		y.l2 = sc.nextDouble();
 		y.l3 = sc.nextDouble();
-		double p = (x.l1 +x.l2 + x.l3) / 2.0;
-		double areaX = Math.sqrt(p * (p - x.l1) * (p - x.l2) * (p - x.l3));
+		p = (x.l1 +x.l2 + x.l3) / 2.0;
+		areaX = Math.sqrt(p * (p - x.l1) * (p - x.l2) * (p - x.l3));
 		p = (y.l1 + y.l2 + y.l3) / 2.0;
-		double areaY = Math.sqrt(p * (p - y.l1) * (p - y.l2) * (p - y.l3));
-		System.out.printf("Triangle X area: %.4f%n", areaX);
-		System.out.printf("Triangle Y area: %.4f%n", areaY);
+		areaY = Math.sqrt(p * (p - y.l1) * (p - y.l2) * (p - y.l3));
+		System.out.printf("Triangle X area: %.4f\n", areaX);
+		System.out.printf("Triangle Y area: %.4f\n", areaY);
 		if (areaX > areaY) {
 			System.out.println("Larger area: X");
 		} else {
