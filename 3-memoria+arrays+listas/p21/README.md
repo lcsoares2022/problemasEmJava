5ca34cd0e9f5b9f2babccf5330c1c7b5fe808367
Lucas Soares <polonio230@hotmail.com>
2023-08-07 15:35:53 -0300

    Adicionando problema p21 de media dos pares
    Aqui foi feito uso do operador ternário no argumento do método de impressão `println`.

---
+++ 3-memoria+arrays+listas/p21/README.md
@@ -0,0 +1,12 @@
+# Média pares
+
+Fazer um programa para ler um vetor de N números inteiros. Em seguida, mostrar na tela a média aritmética somente dos números pares lidos, com uma casa decimal. Se nenhum número par for digitado, mostrar a mensagem "NENHUM NUMERO PAR"
+
+## Exemplo 1
+
+![Exemplo](../../imagens/img14.png)
+
+## Exemplo 2
+
+![Exemplo](../../imagens/img15.png)
+
---
+++ 3-memoria+arrays+listas/p21/src/App.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        int soma=0, qtdPares=0;
+        double media;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Quantos elementos vai ter o vetor? ");
+        int N = sc.nextInt();
+        int[] vect = new int[N];
+
+        for(int i = 0; i < vect.length; i++) {
+            System.out.print("Digite um numero: ");
+            vect[i] = sc.nextInt();
+
+            if(vect[i] % 2 == 0) {
+                soma += vect[i];
+                qtdPares++;
+            }
+        }
+        System.out.println(soma != 0 ? "MEDIA DOS PARES = " + (double) soma / qtdPares : "NENHUM NUMERO PAR");
+        sc.close();
+    }
+}
