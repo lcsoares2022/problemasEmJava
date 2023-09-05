0fb60a0fd5e990209389968cbd22e5e6890d47ae
Lucas Soares <polonio230@hotmail.com>
2023-09-05 10:16:22 -0300

    feat: define package and class structure

---
+++ 4-enumeracao-composicao/p28/src/entities/Worker.java
@@ -0,0 +1,51 @@
+package entities;
+
+import enums.WorkerLevel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Worker {
+    private String name;
+    private WorkerLevel level;
+    private Double baseSalary;
+    private Department department;
+    private List<HourContract> contracts;
+
+    public Worker(String name, WorkerLevel level, Double baseSalary, Department department) {
+        this.name = name;
+        this.level = level;
+        this.baseSalary = baseSalary;
+        this.department = department;
+        contracts = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public WorkerLevel getLevel() {
+        return level;
+    }
+
+    public void setLevel(WorkerLevel level) {
+        this.level = level;
+    }
+
+    public Double getBaseSalary() {
+        return baseSalary;
+    }
+
+    public void setBaseSalary(Double baseSalary) {
+        this.baseSalary = baseSalary;
+    }
+
+
+
+
+
+}
---
+++ 4-enumeracao-composicao/p28/src/enums/WorkerLevel.java
@@ -0,0 +1,7 @@
+package enums;
+
+public enum WorkerLevel {
+    JUNIO,
+    MID_LEVEL,
+    SENIOR
+}
