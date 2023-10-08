8e6c4e078f3a97a6ee76fd14f03a1ce33b2a0d99
Lucas Soares <polonio230@hotmail.com>
2023-10-08 10:15:18 -0300

    code: add the Product superclass and its subclasses ImportedProduct and UsedProduct

--- 5-heranca-polimorfismo/p31/src/entities/UsedProduct.java
+++ 5-heranca-polimorfismo/p31/src/entities/UsedProduct.java
@@ -1,2 +1,41 @@
-package entities;public class UsedProduct {
+package entities;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public final class UsedProduct extends Product{
+
+    private LocalDate manufactureDate;
+
+    private static final DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+    public UsedProduct() {
+        super();
+    }
+
+    public UsedProduct(String name, Double price, LocalDate manufactureDate) {
+        super(name, price);
+        this.manufactureDate = manufactureDate;
+    }
+
+
+    public LocalDate getManufactureDate() {
+        return manufactureDate;
+    }
+
+    public void setManufactureDate(LocalDate manufactureDate) {
+        this.manufactureDate = manufactureDate;
+    }
+
+
+    @Override
+    public String priceTag() {
+        return this.getName()
+             + " (used) "
+             + "$ "
+             + this.getPrice()
+             + "Manufacture date: "
+             + this.getManufactureDate().format(fmt);
+    }
+
 }
