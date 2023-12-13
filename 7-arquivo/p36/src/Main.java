5b69c9e7ef4b1ee1c5f38458510e513b9c4478d5
Lucas Soares <polonio230@hotmail.com>
2023-12-12 21:04:17 -0300

    refactor: melhorar legibilidade e modularização do código

--- 7-arquivo/p36/src/Main.java
+++ 7-arquivo/p36/src/Main.java
@@ -1,18 +1,15 @@
 import java.io.*;
 import java.util.Locale;
 
-
-/**
- * Esta classe cria um arquivo e gera um subdiretório com um arquivo
- * @author Lucas Cabral Soares
- */
 public class Main {
+    private static final String INPUT_PATH = "L:\\repositories\\programacao-orientada-a-objetos\\7-arquivo\\p36\\in.csv";
+    private static final String OUTPUT_DIRECTORY_PATH = "L:\\repositories\\programacao-orientada-a-objetos\\7-arquivo\\p36\\out";
+    private static final String OUTPUT_FILE_PATH = OUTPUT_DIRECTORY_PATH + File.separator + "summary.csv";
+
     public static void main(String[] args) {
-
-        //todo(1): criar arquivo e inserir dados nele
-
         Locale.setDefault(Locale.US);
 
+        // Criar arquivo e inserir dados nele
         String[] lines = new String[] {
                 "TV LED, 1290.99, 1",
                 "Video Game Chair, 350.50, 3",
@@ -20,20 +17,32 @@ public class Main {
                 "Samsung Galaxy 9, 850.00, 2"
         };
 
-        String path = "L:\\repositories\\programacao-orientada-a-objetos\\7-arquivo\\p36\\in.csv";
+        fileWriter(INPUT_PATH, lines);
 
-        fileWriter(path, lines);
+        // Ler arquivo e capturar dados
+        String[] newLines = processFile(INPUT_PATH);
 
+        // Fazer pasta out e um arquivo dentro dela para armazenar o resultado
+        createOutputDirectory();
+        fileWriter(OUTPUT_FILE_PATH, newLines);
+    }
 
-        //todo(2): ler arquivo e capturar dados
+    private static void createOutputDirectory() {
+        File directory = new File(OUTPUT_DIRECTORY_PATH);
+        if (directory.mkdir()) {
+            System.out.println("Folder created: " + directory.getAbsolutePath());
+        }
+    }
 
+    private static String[] processFile(String path) {
         String[] newLines = new String[4];
 
-        try(BufferedReader br = new BufferedReader(new FileReader(path))){
+        try (BufferedReader br = new BufferedReader(new FileReader(path))) {
 
             String line = br.readLine();
             int i = 0;
-            while(line != null) {
+
+            while (line != null) {
                 String[] vetLine = line.split(",\\s+");
                 double result = Double.parseDouble(vetLine[vetLine.length - 1]) * Double.parseDouble(vetLine[vetLine.length - 2]);
                 newLines[i] = String.format(vetLine[0] + ", %.2f", result);
@@ -41,47 +50,19 @@ public class Main {
                 line = br.readLine();
             }
         } catch (IOException e) {
-            System.out.println("Erro: " + e.getMessage());
+            System.out.println("Error processing file: " + e.getMessage());
         }
-
-
-        //todo(3): fazer pasta out e um arquivo dentro dela para armazenar o resultado
-
-        String pathNewDirectory = "L:\\repositories\\programacao-orientada-a-objetos\\7-arquivo\\p36" + "\\out";
-
-        File pathFile = new File(pathNewDirectory);
-
-        boolean sucess = pathFile.mkdir();
-
-        String pathNewFile = "L:\\repositories\\programacao-orientada-a-objetos\\7-arquivo\\p36" + "\\out" +
-                "\\summary.csv";
-
-        fileWriter(pathNewFile, newLines);
-
-
+        return newLines;
     }
 
-
-    /**
-     * Este método escreve em um arquivo.
-     * @param path
-     * @param lines
-     */
     public static void fileWriter(String path, String[] lines) {
-
-        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
-
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(path))) {
             for (String line : lines) {
                 bw.write(line);
                 bw.newLine();
             }
         } catch (IOException e) {
-            e.printStackTrace();
+            System.out.println("Error writing to file: " + e.getMessage());
         }
-
     }
-
-
-
-
 }
