fcb18dbe24f9af6f9befec3c1474d1cee2eca537
Lucas Soares <polonio230@hotmail.com>
2024-01-26 20:26:33 -0300

    feat: solve p44 problem

--- 10-funcional+lambda/p44/src/Main.java
+++ 10-funcional+lambda/p44/src/Main.java
@@ -3,12 +3,12 @@ import models.Product;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter full file path: ");
         String path = sc.nextLine();
@@ -21,17 +21,28 @@ public class Main {
                 String[] data = line.split(",");
                 Product product = new Product(data[0],Double.parseDouble(data[1]));
                 products.add(product);
+                line = br.readLine();
             }
 
+
+            OptionalDouble average = products.stream()
+                    .mapToDouble(Product::getPrice)
+                    .average();
+
+            double result = average.orElse(0.0);
+
+            Comparator<Product> comp = (p1,p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+            List<Product> belowAverage = products.stream()
+                    .filter(x -> x.getPrice() < result)
+                    .sorted(comp.reversed()).toList();
+
+            System.out.println("Average price: " + String.format("%.2f", result));
+            belowAverage.forEach(x -> System.out.println(x.getName()));
 
         } catch (IOException e) {
             System.out.println("Error processing file: " + e.getMessage());
         }
-
-
-
-
-
 
 
         sc.close();
@@ -40,3 +51,6 @@ public class Main {
 
     }
 }
+
+
+// todo(1):
